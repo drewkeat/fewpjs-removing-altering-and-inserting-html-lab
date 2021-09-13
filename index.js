ca1c4c49c822ef449815fc11b5b8c16a49df646a
Drew Keat <thedrewkeat@gmail.com>
2021-09-13 16:12:47 -0600

    commit tree 1e8d01474f9f471c754aa0b923bfffd3efafce41

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+
+document.getElementById("main").remove()
+
+// index.html after index.js is processed
+// has a 'newHeader' variable that points to node 'h1#victory':
+
+let newHeader = document.createElement("h1");
+newHeader.id = 'victory';
+newHeader.innerHTML = "Drew is the champion.";
+document.body.appendChild(newHeader);
